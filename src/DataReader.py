313a013bb25d6f38245dbabda342aea52b341c0f
VladislavGrom1 <vaganov.vladislav2015@yandex.ru>
2025-10-11 17:20:35 +0300

    Форматирование кода

--- src/DataReader.py
+++ src/DataReader.py
@@ -2,6 +2,8 @@
 from Types import DataType
 from abc import ABC, abstractmethod
 
+
 class DataReader(ABC):
- @abstractmethod
- def read(self, path: str) -> DataType:pass
+    @abstractmethod
+    def read(self, path: str) -> DataType:
+        pass
